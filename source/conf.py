8111ac39166d245f51a6e3710694bdeea7feb32d
Clemie McCartney <git@gnztmpz.eu.org>
2022-11-30 00:29:01 +0700

    genesis

---
+++ source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import revitron_sphinx_theme
+import myst_parser
+
+project = 'Madeline'
+copyright = '2022 - present, MatthewSoft'
+author = 'Clemie McCartney'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "revitron_sphinx_theme",]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "revitron_sphinx_theme"
+html_static_path = ['_static']
+html_theme_options = {
+    'color_scheme': 'dark'
+}
+html_context = {
+    'landing_page': {
+        'menu': [
+            {'title': 'Get Started', 'url': 'installing.html'},
+            {'title': 'GitHub', 'url': 'https://github.com/user/repo'}
+        ]
+    }
+}
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. madeline-docs documentation master file, created by
+   sphinx-quickstart on Wed Nov 30 00:14:49 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to madeline-docs's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
