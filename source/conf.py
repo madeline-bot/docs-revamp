356f86f58ccce3c23118a8254beb5a848d123532
Clemie McCartney <git@gnztmpz.eu.org>
2022-12-03 01:28:59 +0700

    better config

--- source/conf.py
+++ source/conf.py
@@ -3,10 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+master_doc = 'index'
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import revitron_sphinx_theme
-import myst_parser
 
 project = 'Madeline'
 copyright = '2022 - present, MatthewSoft'
@@ -15,30 +16,38 @@ author = 'Clemie McCartney'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "revitron_sphinx_theme",]
+extensions = ["revitron_sphinx_theme",]
 
 templates_path = ['_templates']
 exclude_patterns = []
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-}
+
+# Open Graph extension config. https://pypi.org/project/sphinxext-opengraph/
+ogp_site_url = "https://www.madeline.my.id/"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "revitron_sphinx_theme"
+html_logo = '_static/madeline.png'
+html_title = 'Madeline'
+html_favicon = '_static/madeline.png'
 html_static_path = ['_static']
 html_theme_options = {
+    'github_url': 'https://github.com/madeline-bot',
     'color_scheme': 'dark'
 }
 html_context = {
     'landing_page': {
-        'menu': [
-            {'title': 'Get Started', 'url': 'installing.html'},
-            {'title': 'GitHub', 'url': 'https://github.com/user/repo'}
-        ]
+        'menu': [{
+            'title': 'Home',
+            'url': 'index.html'
+        }, {
+            'title': '♡ Sponsor',
+            'url': 'https://github.com/sponsors/madeline-bot'
+        }, {
+            'title': 'Vote us on top.gg',
+            'url': 'https://top.gg/bot/859991918800011295'
+        }]
     }
 }
