876b3b69875dbe77391d05fc3a007d1eaf497298
Clemie McCartney <git@gnztmpz.eu.org>
2022-12-06 14:34:11 +0700

    remove unused things

--- source/conf.py
+++ source/conf.py
@@ -21,21 +21,8 @@ author = 'Clemie McCartney'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
     'sphinxext.opengraph',
 ]
-
-napoleon_google_docstring = True
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = True
-napoleon_use_admonition_for_references = True
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = True
 
 templates_path = ['_templates']
 exclude_patterns = []
